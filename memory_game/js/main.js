9703ec0458adf1313292713b03710a8b2e74aa6a
Keith <ozmaneku@gmail.com>
2019-01-13 15:58:08 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,24 +2,22 @@ var cards=["queen", "queen", "king", "king"];
 
 var cardsInPlay=[];
 
-var cardOne=cards[0];
-
-cardsInPlay.push(cardOne);
-
-var cardTwo=cards[1];
-
-cardsInPlay.push (cardTwo);
-
-console.log("user flipped " + cardsInPlay);
-
-if (cardsInPlay.length === 2)
-{
-	if (cardsInPlay[0]===cardsInPlay[1])
-	{
-		alert("You got a match!");
-	}
-	else
-	{
-		alert("Sorry, try again.");
+var checkForMatch=function(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
+	} else {
+		console.log("Sorry, try again.");
 	}
 }
+
+var flipCard=function(cardId) {
+
+	console.log("user flipped " + cards[cardId]);
+
+	cardsInPlay.push(cards[cardId]);
+
+	checkForMatch();
+}
+
+flipCard(0);
+flipCard(1);
